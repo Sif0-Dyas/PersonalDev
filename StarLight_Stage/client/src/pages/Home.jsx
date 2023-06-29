4e3a33ebdc1eacc552464ef468475cfb8219ede5
Andres Avalos <mrandresavalos@gmail.com>
2023-06-28 17:16:01 -0700

    slight change to code

--- StarLight_Stage/client/src/pages/Home.jsx
+++ StarLight_Stage/client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
 
   const [eventList, setEventList] = useState([])
-  const [loaded, ] = useState(false)
+  const [loaded,] = useState(false)
 
   useEffect(() => {
     axios.get('http://localhost:8000/api/events')
@@ -23,37 +23,16 @@ const Home = () => {
       .catch((error) => { console.log("This is an error", error) })
   }, [loaded])
 
-  // const handleDelete = (e, id) => {
-  //   axios.delete(`http://localhost:8000/api/event/${id}`)
-  //     .then((res) => {
-  //       console.log('Deleting this event response:', id)
-  //       setLoaded(!loaded)
-  //     })
-  //     .catch((error) => { console.log("This is handle error", error) })
-
-  // }
-
-  // useEffect(() => {
-  //   axios.get('/api/getUser')
-  //     .then(res => {
-  //       if (res.data) {
-  //         setUser(res.data);
-  //       } else {
-  //         console.error("No user data received");
-  //       }
-  //     })
-  //     .catch(err => console.error(err));
-
   return (
     <div className='full'>
-    <div>
-      <SideBar/>
+      <div>
+        <SideBar />
 
-   
+
         <div className="homeBox">
           <section className='homeSection black'>
             <h1 className='homeH1'>Welcome {user.firstName}</h1>
-            <Profile/>
+            <Profile />
           </section>
 
 
@@ -63,61 +42,61 @@ const Home = () => {
 
 
 
-          <div className="max-w-7xl mx-auto">
+            <div className="max-w-7xl mx-auto">
               <div className="overflow-x-auto shadow-md sm:rounded-lg">
-                    <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
-                      <thead>
-                        <tr>
-                          <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Name</th>
-                          <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Start Date</th>
-                          <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">End Date</th>
-                          <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Venue Name</th>
-                          <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Country</th>
-                          <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Rating</th>
-                          <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Top 10</th>
-                          <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Actions</th>
-                        </tr>
-                      </thead>
+                <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                  <thead>
+                    <tr>
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Name</th>
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Start Date</th>
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">End Date</th>
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Venue Name</th>
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Country</th>
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Rating</th>
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Top 10</th>
+                      <th className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Actions</th>
+                    </tr>
+                  </thead>
 
-                      <tbody>
-                        {
-                          eventList.map((event, i) => {
-                            return (
-                              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={i}>
-                                <td className="px-6 py-4">{event.name}</td>
-                                <td className="px-6 py-4">{new Date(event.startDate).toLocaleDateString()}</td>
-                                <td className="px-6 py-4">{new Date(event.endDate).toLocaleDateString()}</td>
-                                <td className="px-6 py-4">{event.venueName}</td>
-                                <td className="px-6 py-4">{event.country}</td>
-                                <td className="px-6 py-4">{event.rating}</td>
-                                <td className="px-6 py-4">{event.top10 ? "Yes" : "No"}</td>
-                                <td className="px-6 py-4">
-                                  <button className="btn btn-outline-dark"><Link to={`/details/${event._id}`}>View</Link></button> 
-                                  {/* <button className="btn btn-outline-warning" ><Link to={`/edit/${event._id}`}>Edit</Link></button> | */}
-                                  {/* <button onClick={(e) => { handleDelete(e, event._id) }} className="btn btn-outline-danger">Delete</button> */}
-                                </td>
-                              </tr>
-                            )
-                          })
-                        }
-                      </tbody>
+                  <tbody>
+                    {
+                      eventList.map((event, i) => {
+                        return (
+                          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={i}>
+                            <td className="px-6 py-4">{event.name}</td>
+                            <td className="px-6 py-4">{new Date(event.startDate).toLocaleDateString()}</td>
+                            <td className="px-6 py-4">{new Date(event.endDate).toLocaleDateString()}</td>
+                            <td className="px-6 py-4">{event.venueName}</td>
+                            <td className="px-6 py-4">{event.country}</td>
+                            <td className="px-6 py-4">{event.rating}</td>
+                            <td className="px-6 py-4">{event.top10 ? "Yes" : "No"}</td>
+                            <td className="px-6 py-4">
+                              <button className="btn btn-outline-dark"><Link to={`/details/${event._id}`}>View</Link></button>
+                              {/* <button className="btn btn-outline-warning" ><Link to={`/edit/${event._id}`}>Edit</Link></button> | */}
+                              {/* <button onClick={(e) => { handleDelete(e, event._id) }} className="btn btn-outline-danger">Delete</button> */}
+                            </td>
+                          </tr>
+                        )
+                      })
+                    }
+                  </tbody>
 
-                    </table>
+                </table>
 
               </div>
             </div>
           </div>
-                        </div>
-                    
-          <div className="homeBox">
-            <section className='homeSection black'>
-              <h1 >Supplementary area.</h1>
-            </section>
-          </div>
+        </div>
+
+        <div className="homeBox">
+          <section className='homeSection black'>
+            <h1 >Supplementary area.</h1>
+          </section>
+        </div>
 
       </div>
-      </div>
-  
+    </div>
+
   )
 }
 
